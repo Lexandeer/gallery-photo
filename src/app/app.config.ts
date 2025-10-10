156b9e9e662594faeca5926d69e30697ddadd9d9
Alexandre <alexmiquel888@gmail.com>
2025-10-10 18:46:48 +0200

    Creation of the components + routing pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "gallery-photo-64a97", appId: "1:1012892453987:web:de2e424e9e9b97789bfc56", storageBucket: "gallery-photo-64a97.firebasestorage.app", apiKey: "AIzaSyBEjFBpWDQqcP2TfIbp5NnvJ5t9wqq755A", authDomain: "gallery-photo-64a97.firebaseapp.com", messagingSenderId: "1012892453987", projectNumber: "1012892453987", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Gallery } from './pages/gallery/gallery';
+import { Details } from './pages/details/details';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: Home},
+  {path: 'gallery', component: Gallery},
+  {path: 'dashboard', component: Dashboard},
+  {path: 'details', component: Details},
+];

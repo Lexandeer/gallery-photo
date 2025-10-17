1e0f49cfb660b3a07202b48d163b6b7a078e974b
Alexandre <alexmiquel888@gmail.com>
2025-10-17 12:21:44 +0200

    feat: Add CRUD methods from galleryService and modified template to match code

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { getStorage, ref, uploadBytes, getDownloadURL } from '@angular/fire/storage';
+import { Router } from '@angular/router';
+import { Photo } from '../../models/photo.model';
+import { GalleryService } from '../../services/gallery';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from "@angular/common"
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   photoForm: FormGroup;
   selectedFile: any;
+  isEditing: boolean = false
+  photoData!: Observable<Photo[]>;
 
-  constructor(private fb: FormBuilder) {
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private galleryService: GalleryService
+  ) {
     // Initialisation du formulaire réactif avec des contrôles et des validateurs
     this.photoForm = this.fb.group({
       id: [null],
@@ -50,9 +66,67 @@ export class Dashboard {
     this.selectedFile = event.target.files[0];
   }
 
-  // Soumission du formulaire (Pour l'instant on ne fait que loguer les valeurs)
-  onSubmit():void {
-    console.log(this.photoForm.value)
+
+  // onSubmit():void {
+  //  console.log(this.photoForm.value)
+  // }
+
+  saveData() {
+    if (this.photoForm.valid) {
+      const formData: Photo = this.photoForm.value;
+      if (this.selectedFile) {
+        // S'il y a une image selectionnée, on la stocke d'abord sur Firebase Storage
+        const storage = getStorage();
+        const storageRef = ref(storage, 'images/' + this.selectedFile.name);
+        uploadBytes(storageRef, this.selectedFile).then((snapshot) => {
+          getDownloadURL(snapshot.ref).then((downloadURL) => {
+            formData.imageUrl = downloadURL;
+            this.galleryService.savePhoto(formData).then(() => {
+              this.resetForm();
+            });
+          });
+        });
+      } else {
+        // Si aucune image n'a été selectionnée, on enregistre juste les données (imageUrl éventuellement vide)
+        this.galleryService.savePhoto(formData).then( () => {
+          this.resetForm();
+        });
+      }
+    } else {
+      console.log('All field are required');
+    }
   }
 
+  // Récupérer la liste des photos pour affichage
+  getData() {
+    this.photoData = this.galleryService.getPhotos();
+  }
+
+  // Remplir le formulaire avec les données d'une photo existante (pour édition)
+  editData(data: Photo) {
+    this.isEditing = true;
+    this.photoForm.patchValue(data)
+  }
+
+  //Supprimer une photo
+  deleteData(id: string) {
+    if (confirm('Etes vous sûr de vouloir supprimer cet élément ?')) {
+      this.galleryService.deletePhoto(id).catch(error => {
+        console.error("error deleting photo:", error);
+        alert('Une erreur s est produit lors de la suppression de la photo');
+      })
+    }
+  }
+
+  //Réinitialiser le formulaire après enregistrement ou annulation
+  resetForm() {
+    this.photoForm.reset({ likes: 0, imageUrl: '', description: '', title: '' });
+    this.selectedFile = null
+    this.isEditing = false
+  }
+
+  // Naviguer vers la page de détails pour une photo donnée
+  navigateToDetails(id: string) {
+    this.router.navigate(['/details', id])
+  }
 }

2776afa70c53b9288af04ef5c49823bce06c4660
Alexandre <alexmiquel888@gmail.com>
2025-10-21 17:37:10 +0200

    feat: Add a gallery page to display photos

--- src/app/pages/gallery/gallery.ts
+++ src/app/pages/gallery/gallery.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router'
+import { Firestore, collection, collectionData }  from '@angular/fire/firestore'
+import { Observable, map } from 'rxjs'
+import { Photo } from '../../models/photo.model'
+import { GalleryService } from '../../services/gallery'
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [RouterModule, AsyncPipe],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css'
 })
-export class Gallery {
+export class Gallery implements OnInit {
+  photoData!: Observable<Photo[]>;
+  selectedImage: Photo | null = null;
 
+  constructor(
+    private firestore: Firestore,
+    private router: Router,
+    private galleryService: GalleryService
+  ) {}
+
+  ngOnInit() {
+    this.getData()
+  }
+
+   getData() {
+      const collectionInstance = collection(this.firestore, 'photos');
+      this.photoData = collectionData(collectionInstance, {idField: 'id'}).pipe(
+        map(dataArray =>
+          dataArray.map(data => ({
+            id: data['id'],
+            title: data['title'],
+            description: data['description'],
+            imageUrl: data['imageUrl'],
+            likes: data['likes']
+          }) as Photo )
+        )
+      );
+   }
+
+  onLikes(photo: Photo) {
+    this.galleryService.likePhoto(photo)
+  }
+
+  navigateToDetails(id: string) {
+    this.router.navigate(['/details', id]);
+  }
 }

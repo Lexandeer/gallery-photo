0072addf091a6c073c858ab82aabcfda9e5f985e
Alexandre <alexmiquel888@gmail.com>
2025-10-21 17:36:25 +0200

    feat: Add a details page for the photos

--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router'
+import { Firestore, doc, getDoc } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './details.html',
   styleUrl: './details.css'
 })
-export class Details {
+export class Details implements OnInit {
+ data: any; // Contiendra les informations de la photo à afficher
 
+  constructor(
+    private route: ActivatedRoute,
+    private fireStore: Firestore
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null ) {
+      const docRef = doc(this.fireStore, 'photos', id);
+      getDoc(docRef).then((docData) => {
+        if (docData.exists()) {
+          this.data = docData.data();
+        } else {
+          console.log('No document found');
+        }
+      });
+    }
+  }
 }
